94af5d1b2dce26a27a3222d5d31792c5adc67f12
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2025-08-20 15:34:24 +0545

    Add workflow to publish package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='adaup',
-    version='0.1.0',
+    version='0.1.1',
     packages=['adaup'],
     package_dir={'': 'src'},
     install_requires=[
