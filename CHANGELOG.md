50c2d2ccb5b8563ea2c7daa26de01dea4bc24f52
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2025-08-20 15:50:33 +0545

    Fix release build

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+
+## Version 0.1.2
+
+### Fixed
+- `f6924a1` - Fix release build
+
+## Version 0.1.1
+
+### Added
+- `94af5d1` - Add workflow to publish package
+
+## Version 0.1.0
+
+### Added
+- `692d7f9` - Initial version
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name='adaup',
-    version='0.1.1',
-    packages=['adaup'],
+    version='0.1.2',
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
         'tqdm',
